18a42bcd4d6d895a6feb3673c3bfdac57e650fe3
梁信众 <liangxinzhong@liangxinzhongdeMacBook-Pro.local>
2017-04-16 15:00:19 +0800

    practice 20170416

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,45 @@
+class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    flash[:notice] = "Update success."
+    redirect_to jobs_path
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.save
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job deleted!"
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
