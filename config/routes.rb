980e73ea31979edae31197968b1c1f8f70f5ef84
梁信众 <liangxinzhong@liangxinzhongdeMacBook-Pro.local>
2017-04-19 11:21:21 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   resources :jobs do
     resources :resumes
   end
+
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
   root "jobs#index"
