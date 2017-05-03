ff9cb91a41cb7821f03094bda4482bb73a320b44
梁信众 <liangxinzhong@liangxinzhongdeMacBook-Pro.local>
2017-05-04 07:03:33 +0800

    add white list for admin jobs controller

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -67,6 +67,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :city)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :city, :category, :company)
   end
 end
